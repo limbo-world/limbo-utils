f87150febb27ed474a9e488c5ad1e736e365babd
brozen <brozen.lau@gmail.com>
2022-01-04 19:21:12 +0800

    modify 调整InstantUtils工具接口

--- src/main/java/org/limbo/utils/instant/InstantUtils.java
+++ src/main/java/org/limbo/utils/instant/InstantUtils.java
@@ -18,9 +18,9 @@
 
 package org.limbo.utils.instant;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.time.ZoneId;
+import lombok.experimental.UtilityClass;
+
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,105 +29,210 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Brozen
  * @since 1.0
  */
+@UtilityClass
 public class InstantUtils {
 
-    private static final String YMD_HMS = "yyyy-MM-dd HH:mm:ss";
-    private static final String YMD = "yyyy-MM-dd";
-    private static final String HMS = "HH:mm:ss";
+    public static final String YMD_HMS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String YMD_HMS = "yyyy-MM-dd HH:mm:ss";
+    public static final String YMD = "yyyy-MM-dd";
+    public static final String HMS = "HH:mm:ss";
 
     /**
-     * DEFAULT_ZONE 通过环境变量“limbo.instant.zone”初始化，默认UTC+8东八区
+     * 默认使用的时区，与{@link #DEFAULT_ZONE_OFFSET}保持一致。
      */
-    static final ZoneId DEFAULT_ZONE;
+    public static final ZoneId DEFAULT_ZONE;
+
+    /**
+     * 时区偏移量，可通过环境变量“limbo.instant.zone-offset”初始化，默认+8东八区。
+     */
+    public static final ZoneOffset DEFAULT_ZONE_OFFSET;
+
     static final Map<String, DateTimeFormatter> FORMATTERS = new ConcurrentHashMap<>();
     static {
-        String zoneId = System.getProperty("limbo.instant.zone", "UTC+8");
-        DEFAULT_ZONE = ZoneId.of(zoneId);
+        String zoneOffset = System.getProperty("limbo.instant.zone-offset", "+8");
+        DEFAULT_ZONE = DEFAULT_ZONE_OFFSET = ZoneOffset.of(zoneOffset);
 
         FORMATTERS.put(YMD_HMS, DateTimeFormatter.ofPattern(YMD_HMS).withZone(DEFAULT_ZONE));
         FORMATTERS.put(YMD, DateTimeFormatter.ofPattern(YMD).withZone(DEFAULT_ZONE));
         FORMATTERS.put(HMS, DateTimeFormatter.ofPattern(HMS).withZone(DEFAULT_ZONE));
     }
 
-    private static DateTimeFormatter getFormatter(String pattern, ZoneId zone) {
-        return FORMATTERS.computeIfAbsent(pattern, _pattern -> DateTimeFormatter.ofPattern(_pattern).withZone(zone));
-    }
 
+    /**
+     * 获取指定pattern的格式化器
+     */
     private static DateTimeFormatter getFormatter(String pattern) {
-        return getFormatter(pattern, DEFAULT_ZONE);
+        return FORMATTERS.computeIfAbsent(pattern, DateTimeFormatter::ofPattern);
     }
 
+
+    /**
+     * 将{@link Instant}格式化为指定格式，并格式化时进行时区转换。
+     * @param instant 时间戳
+     * @param pattern 日期格式
+     * @param zone 格式化前，先将时间戳转换为指定时区，再进行格式化
+     * @return 时间戳格式化后的字符串
+     */
     public static String format(Instant instant, String pattern, ZoneId zone) {
-        DateTimeFormatter formatter = getFormatter(pattern);
+        DateTimeFormatter formatter = getFormatter(pattern).withZone(zone);
         return formatter.format(instant);
     }
 
+
+    /**
+     * 将{@link Instant}格式化为指定格式，并格式化时进行时区转换。
+     * @param instant 时间戳
+     * @param pattern 日期格式
+     * @param zone 格式化前，先将时间戳转换为指定时区，再进行格式化
+     * @return 时间戳格式化后的字符串
+     */
     public static String format(Instant instant, String pattern, String zone) {
         return format(instant, pattern, ZoneId.of(zone));
     }
 
+
+    /**
+     * 将{@link Instant}格式化为指定格式，并使用默认时区{@link #DEFAULT_ZONE}进行格式化。
+     * @param instant 时间戳
+     * @param pattern 日期格式
+     * @return 时间戳格式化后的字符串
+     */
     public static String format(Instant instant, String pattern) {
         return format(instant, pattern, DEFAULT_ZONE);
     }
 
+
+    /**
+     * 将{@link Instant}格式化为"<code>yyyy-MM-dd HH:mm:ss</code>"格式，并使用默认时区{@link #DEFAULT_ZONE}进行格式化。
+     * @param instant 时间戳
+     * @return 时间戳格式化后的字符串
+     */
     public static String formatYMDHMS(Instant instant) {
         return FORMATTERS.get(YMD_HMS).format(instant);
     }
 
+
+    /**
+     * 将{@link Instant}格式化为"<code>yyyy-MM-dd</code>"格式，并使用默认时区{@link #DEFAULT_ZONE}进行格式化。
+     * @param instant 时间戳
+     * @return 时间戳格式化后的字符串
+     */
     public static String formatYMD(Instant instant) {
         return FORMATTERS.get(YMD).format(instant);
     }
 
+
+    /**
+     * 将{@link Instant}格式化为"<code>HH:mm:ss</code>"格式，并使用默认时区{@link #DEFAULT_ZONE}进行格式化。
+     * @param instant 时间戳
+     * @return 时间戳格式化后的字符串
+     */
     public static String formatHMS(Instant instant) {
         return FORMATTERS.get(HMS).format(instant);
     }
 
+
+    /**
+     * 将格式化日期字符串转换为{@link Instant}，日期字符串的时区通过参数指定。
+     * @param instant 格式化的日期字符串
+     * @param pattern 日期格式
+     * @param zone 日期字符串的时区
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parse(String instant, String pattern, ZoneId zone) {
-        DateTimeFormatter formatter = getFormatter(pattern, zone);
+        DateTimeFormatter formatter = getFormatter(pattern).withZone(zone);
         return Instant.from(formatter.parse(instant));
     }
 
+
+    /**
+     * 将格式化日期字符串转换为{@link Instant}，日期字符串的时区通过参数指定。
+     * @param instant 格式化的日期字符串
+     * @param pattern 日期格式
+     * @param zone 日期字符串的时区
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parse(String instant, String pattern, String zone) {
         return parse(instant, pattern, ZoneId.of(zone));
     }
 
+
+    /**
+     * 将格式化日期字符串转换为{@link Instant}，日期字符串的时区使用默认时区{@link #DEFAULT_ZONE}。
+     * @param instant 格式化的日期字符串
+     * @param pattern 日期格式
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parse(String instant, String pattern) {
         return parse(instant, pattern, DEFAULT_ZONE);
     }
 
+
+    /**
+     * 将"<code>yyyy-MM-dd HH:mm:ss</code>"格式的日期字符串转换为{@link Instant}，日期字符串的时区使用默认时区{@link #DEFAULT_ZONE}。
+     * @param instant 格式化的日期字符串
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parseYMDHMS(String instant) {
         return Instant.from(FORMATTERS.get(YMD_HMS).parse(instant));
     }
 
+
+    /**
+     * 将"<code>yyyy-MM-dd</code>"格式的日期字符串转换为{@link Instant}，日期字符串的时区使用默认时区{@link #DEFAULT_ZONE}。
+     * @param instant 格式化的日期字符串
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parseYMD(String instant) {
         return Instant.from(FORMATTERS.get(YMD).parse(instant));
     }
 
+
+    /**
+     * 将"<code>HH:mm:ss</code>"格式的日期字符串转换为{@link Instant}，日期字符串的时区使用默认时区{@link #DEFAULT_ZONE}。
+     * @param instant 格式化的日期字符串
+     * @return 日期字符串对应的时间戳
+     */
     public static Instant parseHMS(String instant) {
         return Instant.from(FORMATTERS.get(HMS).parse(instant));
     }
 
-    public static Instant startOfDay(Instant instant) {
-        return parseYMDHMS(formatYMD(instant) + " 00:00:00");
-    }
 
-    public static Instant endOfDay(Instant instant) {
-        return parseYMDHMS(formatYMD(instant) + " 23:59:59");
-    }
-
-    public static Instant startOfToday() {
-        return startOfDay(Instant.now(Clock.system(DEFAULT_ZONE)));
-    }
-
-    public static Instant endOfToday() {
-        return endOfDay(Instant.now(Clock.system(DEFAULT_ZONE)));
+    /**
+     * 获取时间戳所在天的开始时间戳，即年月日保持不变，时分秒均为0的时间戳。
+     * @param instant 时间戳
+     * @return 入参时间戳当天的开始
+     */
+    public static Instant beginningOfDay(Instant instant) {
+        return parse(formatYMD(instant) + " 00:00:00.000", YMD_HMS_SSS);
     }
 
 
-    public static void main(String[] args) {
-        System.setProperty("limbo.instant.zone", "UTC+9");
-        System.out.println(formatYMDHMS(Instant.now()));
-        System.out.println(formatYMDHMS(startOfToday()));
-        System.out.println(formatYMDHMS(endOfToday()));
+    /**
+     * 获取时间戳所在天的结束时间戳，即年月日保持不变，23时59分59秒999毫秒的时间戳。
+     * @param instant 时间戳
+     * @return 入参时间戳当天的结束
+     */
+    public static Instant endingOfDay(Instant instant) {
+        return parse(formatYMD(instant) + " 23:59:59.999", YMD_HMS_SSS);
     }
+
+
+    /**
+     * 获取今天的开始时间戳（按默认时区{@link #DEFAULT_ZONE}获取时间），即年月日保持不变，时分秒均为0的时间戳。
+     * @return 今天的开始
+     */
+    public static Instant beginningOfToday() {
+        return beginningOfDay(Instant.now(Clock.system(DEFAULT_ZONE)));
+    }
+
+
+    /**
+     * 获取今天的结束时间戳（按默认时区{@link #DEFAULT_ZONE}获取时间），即年月日保持不变，23时59分59秒999毫秒的时间戳。
+     * @return 今天的结束
+     */
+    public static Instant endingOfToday() {
+        return endingOfDay(Instant.now(Clock.system(DEFAULT_ZONE)));
+    }
+
 }
---
+++ src/test/java/org/limbo/utils/test/instant/InstantUtilsTest.java
@@ -0,0 +1,62 @@
+package org.limbo.utils.test.instant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import static org.limbo.utils.instant.InstantUtils.*;
+
+/**
+ * @author Brozen
+ * @since 2022-01-04
+ */
+public class InstantUtilsTest {
+
+    @Before
+    public void testZone() {
+        System.out.println("============= 默认时区信息 =============");
+        System.out.println(DEFAULT_ZONE);
+        System.out.println(DEFAULT_ZONE_OFFSET);
+        System.out.println("============= 默认时区信息 [END] =============");
+    }
+
+
+    @Test
+    public void testDefaultZoneAndOffset() {
+//        System.setProperty("limbo.instant.zone-offset", "+9");
+        System.out.println(formatYMDHMS(Instant.now()));
+        System.out.println(formatYMDHMS(beginningOfToday()));
+        System.out.println(formatYMDHMS(endingOfToday()));
+    }
+
+
+    @Test
+    public void testFormat() {
+        Instant now = Instant.now();
+        System.out.println(format(now, "yyyy-MM-dd HH:mm", ZoneOffset.of("+9")));
+        System.out.println(format(now, "yyyy-MM-dd HH:mm"));
+        System.out.println(formatYMDHMS(now));
+        System.out.println(formatYMD(now));
+        System.out.println(formatHMS(now));
+    }
+
+
+    @Test
+    public void testParse() {
+        String nowString = "2022-01-04 18:50";
+        Instant now = parse(nowString, "yyyy-MM-dd HH:mm", ZoneOffset.of("+9"));
+        System.out.println(formatYMDHMS(now));
+    }
+
+
+    @Test
+    public void testBeginningAndEnd() {
+        System.out.println(format(beginningOfToday(), YMD_HMS_SSS));
+        System.out.println(format(endingOfToday(), YMD_HMS_SSS));
+        System.out.println(endingOfToday().toEpochMilli());
+    }
+
+
+}
